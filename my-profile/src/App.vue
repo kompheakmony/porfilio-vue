4f3bcf33c18fae72cb338ae1f94d401b2c5e593f
Nyz Cam <70509420+kompheakmony@users.noreply.github.com>
2025-07-16 09:46:36 +0000

    portfilio public

---
+++ my-profile/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Navbar from '.components/Navbar.vue'
+import Hero from '.components/Hero.vue'
+import Skills from '.components/Skills.vue'
+import Services from '.components/Services.vue'
+import Projects from '.components/Projects.vue'
+import Testimonials from '.components/Testimonials.vue'
+import Contact from '.components/Contact.vue'
+import Footer from '.components/Footer.vue'
+import { onMounted } from "vue";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+onMounted(() => {
+  AOS.init({
+    duration:1800,
+    offset: 100,
+    disable: "mobile",
+  });
+})
+
+</script>
+
+<template>
+  <div class="bg-gray-950">
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Services />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
