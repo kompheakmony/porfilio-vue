516fc9aaebf198b7513d4db44b2831503bc4d87c
Nyz Cam <70509420+kompheakmony@users.noreply.github.com>
2025-07-17 22:33:08 +0700

    Update Navbar.vue

--- my-profile/src/components/Navbar.vue
+++ my-profile/src/components/Navbar.vue
@@ -41,18 +41,37 @@ const updateActive = (index: number) => {
         
       </header>
 
-      <nav
-        class="fixed z-[999] left-1/2 -translate-x-1/2 flex items-center gap-5 bg-yellow-800/60 px-6 py-3 backdrop-blur-md rounded-full text-gray-200 duration-300"
-        :class="{ 'bottom-10': showMenu, 'bottom-[-100%]': !showMenu }"
-      >
-        <a v-for="(item, index) in navItems" :key="index"
-            :href="item.href"
-            @click="updateActive(index)"
-            class="text-xl p-2.5 rounded-full sm:cursor-pointer transition-colors"
-            :class="{ 'bg-yellow-500 text-gray-800': active === index }"
-            >
-            <component :is="item.icon" class="size-6" />
-        </a>
-      </nav>
+      <transition name="navbar-slide-fade">
+          <nav
+            v-if="showMenu"
+            class="fixed z-[999] left-1/2 -translate-x-1/2 flex items-center gap-5 bg-yellow-800/60 px-6 py-3 backdrop-blur-md rounded-full text-gray-200 duration-300 bottom-10"
+          >
+            <a v-for="(item, index) in navItems" :key="index"
+                :href="item.href"
+                @click="updateActive(index)"
+                class="text-xl p-2.5 rounded-full sm:cursor-pointer transition-colors"
+                :class="{ 'bg-yellow-500 text-gray-800': active === index }"
+                >
+                <component :is="item.icon" class="size-6" />
+            </a>
+          </nav>
+        </transition>
     </div>
 </template>
+
+<style scoped>
+.navbar-slide-fade-enter-active,
+.navbar-slide-fade-leave-active {
+  transition: opacity 0.3s, transform 0.3s;
+}
+.navbar-slide-fade-enter-from,
+.navbar-slide-fade-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.navbar-slide-fade-enter-to,
+.navbar-slide-fade-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
